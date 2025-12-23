d1af9c38652707af69df61376f4920c531950c49
umair <umairpak007@gmail.com>
2025-12-23 23:27:10 +0500

    initial commit - project setup

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-print("complaint management system started")
-print("complaint management system")
-print("complaint management system")
-print("complaint management system")
-print("complaint management system")
-
+# import sqlite3
+# sqlite3.connect("database/cms.db")
+# conn = sqlite3.connect("database/cms.db")
+# print("database connection established")
+# conn.close()
+#
+import streamlit as st
+st.set_page_config(page_title="Complaint Management System")
+st.subheader("python final project")
+st.success("system started successfully")
