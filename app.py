182b31ae8836c731a5c8c12c52d37ad9555c5e5e
umair <umairpak007@gmail.com>
2025-12-23 19:19:16 +0500

    initial commit - project setup

---
+++ app.py
@@ -0,0 +1,6 @@
+print("complaint management system started")
+print("complaint management system")
+print("complaint management system")
+print("complaint management system")
+print("complaint management system")
+
