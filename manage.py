d0e938f7aeb4c543656932b5f913941493970e88
umair <umairpak007@gmail.com>
2026-02-15 11:37:14 +0500

    move to vs code

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-# this is main file
+# this is main file of project
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings')
